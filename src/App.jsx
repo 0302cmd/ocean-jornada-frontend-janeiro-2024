4a8f625f6b251c27bc5fb38ccd6c925c986a0915
Yasmim Castro <67880069+0302cmd@users.noreply.github.com>
2024-01-31 14:41:21 +0000

    carregando api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-
+import { useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 
-
 function App() {
+
+  const [itens, setItens] = useState([])
+
+ async function carregarDadosApi(){
+  const apiUrl = 'https://rickandmortyapi.com/api/character/'
+
+  const response= await fetch(apiUrl)
+
+//console.log(response)
+
+  const body = await response.json()
+
+  console.log(body)
+
+  const results = body.results
+
+  console.log(results)
+
+  setItens(results)
+}
+
+carregarDadosApi()
+
   return (
     <>
-     <Card/>
-     <Card/>
+      <div className="cards">
+        {/* <Card item={item1} />
+        <Card item={item2} />
+        <Card item={item3} /> */}
+        {itens.map((item, i) => <Card item={item} key={i} />)}
+      </div>
     </>
   )
 }
